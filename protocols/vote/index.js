62a463dda1cb2acb8d47d50ed751b3aa2e656bf1
Tue Nguyen <tue.nd@techchain.vn>
2024-10-20 16:08:16 +0700

    feat: update providers

--- protocols/vote/index.js
+++ protocols/vote/index.js
@@ -1,19 +1,12 @@
 import { ethers } from "ethers";
 import voteAbi from "./abis/voteAbi.js";
+import { getTaikoProvider } from "../common.js";
 
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.taiko.xyz");
-
-const voteAddress = "0x4D1E2145082d0AB0fDa4a973dC4887C7295e21aB";
-
-const voteContract = new ethers.Contract(voteAddress, voteAbi, provider);
-
-const gasPriceList = ["0.01", "0.02", "0.03", "0.04", "0.05"];
-
-function getRandomGasPrice() {
-  return gasPriceList[Math.floor(Math.random() * gasPriceList.length)];
-}
 async function vote(walletUnconnected) {
   try {
+    const provider = getTaikoProvider();
+    const voteAddress = "0x4D1E2145082d0AB0fDa4a973dC4887C7295e21aB";
+    const voteContract = new ethers.Contract(voteAddress, voteAbi, provider);
     let signer = new ethers.Wallet(walletUnconnected, provider);
 
     const feeData = await provider.getFeeData();
@@ -35,7 +28,7 @@ const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 export async function runVote(userWallet) {
   let wallet = new ethers.Wallet(userWallet);
   console.log(`Start run vote for wallet ${wallet.address}`);
-  for (let i = 0; i < 130; i++) {
+  for (let i = 0; i < 1; i++) {
     console.log("vote ", i + 1);
     await vote(wallet);
     const delayInSeconds = Math.floor(Math.random() * (30 - 10 + 1)) + 10;
